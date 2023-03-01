6dc4b092fdfa23d01e489695d97ce243af53f31b
Bishop <amanda.bishop@cgi.com>
2023-03-01 10:09:17 -0600

    worked through some type narrowing examples and in operator

--- 03_purets/src/detection.ts
+++ 03_purets/src/detection.ts
@@ -15,3 +15,35 @@ function provideId(id:string | null){
     }
     id.toLowerCase();
 }
+
+//don't do this! we haven't covered for an empty string ""
+function printAll(strs: string | string[] | null) {
+    if (strs) {
+      if (typeof strs === "object") {
+        for (const s of strs) {
+          console.log(s);
+        }
+      } else if (typeof strs === "string") {
+        console.log(strs);
+      }
+    }
+  }
+
+//using in operator 
+interface User {
+    name: string, 
+    email: string, 
+}
+
+interface Admin {
+    name: string, 
+    email: string, 
+    isAdmin: boolean
+}
+
+//if prop "isAdmin" is IN the account return account.isAdmin, accounts are User and Admin
+function isAdminAccount(account: User | Admin){
+    if("isAdmin" in account){
+        return account.isAdmin
+    }
+}
